71f6c2c48c26476e47b3f0eb2a82eff10decbbbf
vc <victor.csr@gmail.com>
2025-05-07 19:50:50 +0800

    finish all implementation

--- app.py
+++ app.py
@@ -50,24 +50,12 @@ def index():
 # return render_template("index.html", **locals())
 # @app.route("/filter", methods=["POST"]
 #    county = request.form.get("county")
-
-
-@app.route("/update-db")
-def update_pm25_db():
-    count, message = update_db()
-    nowtime = datetime.now().strftime("%Y-%m-%d")
-    result = json.dumps(
-        {"時間": nowtime, "更新筆數": count, "結果": message}, ensure_ascii=False
-    )
-    return result
-
-
-@app.route("/pm25-county-site")
+@app.route("/pm25-data-site")
 def pm25_data_by_site():
     county = request.args.get("county")
     site = request.args.get("site")
     if not county or not site:
-        result = json.dumps({"error": "胱市跟站點名稱不能為空!"}, ensure_ascii=False)
+        result = json.dumps({"error": "城市跟站點名稱不能為空!"}, ensure_ascii=False)
     else:
         columns, datas = get_pm25_data_by_site(county, site)
         df = pd.DataFrame(datas, columns=columns)
@@ -77,6 +65,8 @@ def pm25_data_by_site():
             "site": site,
             "x_data": df["datacreationdate"].to_list(),
             "y_data": df["pm25"].to_list(),
+            "higher": df["pm25"].max(),
+            "lower": df["pm25"].min(),
         }
     return result
 
@@ -86,6 +76,16 @@ def pm25_county_site():
     county = request.args.get("county")
     sites = get_site_by_counties(county)
     result = json.dumps(sites, ensure_ascii=False)
+    return result
+
+
+@app.route("/update-db")
+def update_pm25_db():
+    count, message = update_db()
+    nowtime = datetime.now().strftime("%Y-%m-%d")
+    result = json.dumps(
+        {"時間": nowtime, "更新筆數": count, "結果": message}, ensure_ascii=False
+    )
     return result
 
 
